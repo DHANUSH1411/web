a152775087538c01adb012d7d41262eef821ba4b
DHANUSH K B <81842333+DHANUSH1411@users.noreply.github.com>
2023-07-19 00:49:45 +0530

    Delete creator-donation-api-main directory

--- creator-donation-api-main/routes/donateRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const donateContoller = require('../controllers/donateConroller')
-
-const router = express.Router();
-
-router.post('/', donateContoller.donate)
-
-module.exports = router;
